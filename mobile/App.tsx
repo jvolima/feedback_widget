b95fc2bb3c489badd6a82150f3f9d8efab9c088d
João Vitor Lima <jvolima2004@gmail.com>
2022-05-07 10:16:11 -0300

    setup inicial mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Widget } from './src/components/Widget';
+import { theme } from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <Widget />
+
+      <StatusBar 
+        style="light" 
+        backgroundColor="transparent"
+        translucent
+      />
+    </View>
+  );
+}
+
