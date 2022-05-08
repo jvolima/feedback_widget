10d584496d2cb55c4af60cc7d83946ca9ee0d048
João Vitor Lima <jvolima2004@gmail.com>
2022-05-08 09:51:41 -0300

    criação do componente Widget e Options

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,10 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
@@ -20,13 +22,13 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
-
       <StatusBar 
         style="light" 
         backgroundColor="transparent"
         translucent
       />
+
+      <Widget />
     </View>
   );
 }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import { TouchableOpacity } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import { ChatTeardropDots } from 'phosphor-react-native';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
+import { Options } from '../Options';
 
-export function Widget() {
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
   return (
-    <View style={styles.container}>
+    <>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={handleOpen}
+      >
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
 
-    </View>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
+    </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
